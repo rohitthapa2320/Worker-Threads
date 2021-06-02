c21ec33aedf306b1a3637937684a84a470250718
rohitthapa2320 <rohitthapa2362000@gmail.com>
2021-06-02 22:56:07 +0530

    Calculated Factorial of a number in worker thread

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const {Worker} = require('worker_threads');
 
+let num=10;
 
 //Creating a new worker thread
-const worker= new Worker('./worker.js');
+const worker= new Worker('./worker.js', {workerData: {num: num}});
 
 //Listening to message from worker thread
-worker.on('message' , message => {
-  console.log('Message Received by Parent: ', message);
+// worker.on('message' , message => {
+//   console.log('Message Received by Parent: ', message);
+// });
+
+//Listening for result from worker
+worker.on('message', result => {
+  console.log("Received result from worker");
+  console.log(`Factorial of ${num}= ${result}`);
 });
 
 //Listening for errors
@@ -16,7 +23,7 @@ worker.on('error', err => {
 
 //Listening for worker thread to exit
 worker.on('exit', exitCode => {
-  console.log("Exit: ", exitCode);
+  console.log("Worker Exited with code: ", exitCode);
 });
 
-console.log("We are in parent thread");
+console.log("\nWe are in parent thread\n");
--- worker.js
+++ worker.js
@@ -1,6 +1,17 @@
-const {parentPort} = require('worker_threads');
+const {parentPort, workerData} = require('worker_threads');
 
-console.log("We are in worker thread");
+console.log("We are in worker thread\n");
 
 //Sending message to main thread
-parentPort.postMessage('Hello from worker thread');
+// parentPort.postMessage('Hello from worker thread');
+
+//Sending result to main thread
+parentPort.postMessage(getFac(workerData.num));
+
+//Fuction to find factorial of a number
+function getFac(number){
+  if(number === 1)
+    return 1;
+
+  return number * getFac(number-1);
+}
